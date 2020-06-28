f8c29eca69ea16562abe07e7094ecc0d7ed1fe79
Jing Wang <99jingw@gmail.com>
2020-06-28 14:13:56 -0400

    Add unit test for cage UI

--- main.test.ts
+++ main.test.ts
@@ -24,6 +24,51 @@ QUnit.test("basic rendering", (assert: any) => {
     assert.ok(root.innerHTML.includes(">X</td>"));
 });
 
+QUnit.test("find", (assert: any) => {
+    const root = document.createElement("div");
+    const ui = new SudokuUI(root);
+
+    ui["textInput"].value = "123456789".repeat(9);
+    const loadButton = root.querySelector("div:nth-child(6) > button:nth-child(1)") as HTMLButtonElement;
+    assert.equal(loadButton.textContent, "Load from text");
+    loadButton.click();
+
+    assert.equal(root.innerHTML.match(/color: rgba\(152, 251, 152, 0\.5\)/g), null);
+    // +1 for the highlight button
+    assert.equal((root.innerHTML.match(/color: rgba\(0, 0, 0, 0\)/g) ?? []).length, 9 * 9 + 1);
+
+    const find2Button = root.querySelector(".find > button:nth-child(2)") as HTMLButtonElement;
+    assert.equal(find2Button.textContent, "2");
+    find2Button.click();
+
+    assert.equal(count(root.innerHTML, /color: rgba\(3, 192, 60, 0\.5\)/g), 9);
+    assert.equal(count(root.innerHTML, /color: rgba\(0, 0, 0, 0\)/g), 9 * 8 + 1);
+});
+
+QUnit.test("select and highlight", (assert: any) => {
+    const root = document.createElement("div");
+    const ui = new SudokuUI(root);
+
+    ui["boardUI"]["_mode"]!.onMouseDown(0, 0, new MouseEvent(""));
+    ui["boardUI"]["_mode"]!.onDrag(1, 0, new MouseEvent(""));
+    ui["boardUI"]["_mode"]!.onDrag(2, 0, new MouseEvent(""));
+
+    assert.equal(count(root.innerHTML, /color: rgba\(255, 215, 0, 0\.5\)/g), 3);
+
+    const highlightButton = root.querySelector(".highlight > button:nth-child(2)") as HTMLButtonElement;
+    highlightButton.click();
+
+    assert.equal(count(root.innerHTML, /color: rgba\(255, 215, 0, 0\.5\)/g), 0);
+    assert.equal(count(root.innerHTML, /color: rgba\(204, 172, 0, 0\.624\)/g), 3);
+
+    ui["boardUI"]["_mode"]!.onMouseDown(8, 8, new MouseEvent(""));
+
+    assert.equal(count(root.innerHTML, /color: rgba\(255, 215, 0, 0\.5\)/g), 1);
+    assert.equal(count(root.innerHTML, /color: rgba\(0, 0, 0, 0\.25\)/g), 3 + 1);
+});
+
+QUnit.module("thermometer UI");
+
 QUnit.test("add thermometer and solve", (assert: any) => {
     const root = document.createElement("div");
     const ui = new SudokuUI(root);
@@ -84,7 +129,7 @@ QUnit.test("add and delete thermometer", (assert: any) => {
     ]);
 });
 
-QUnit.test("delete thermometer during construction", (assert: any) => {
+QUnit.test("abandon thermometer construction", (assert: any) => {
     const root = document.createElement("div");
     const ui = new SudokuUI(root);
 
@@ -96,50 +141,49 @@ QUnit.test("delete thermometer during construction", (assert: any) => {
     assert.deepEqual(mode["collector"]["underConstruction"], [[0, 0], [0, 1]]);
     assert.deepEqual(ui["thermometers"].completed, []);
 
-    (root.querySelectorAll("input[name=mode]")[2] as HTMLInputElement).click();
+    (root.querySelectorAll("input[name=mode]")[0] as HTMLInputElement).click();
     assert.deepEqual(mode["collector"]["underConstruction"], []);
     assert.deepEqual(ui["thermometers"].completed, []);
 });
 
-QUnit.test("find", (assert: any) => {
+QUnit.module("cage UI");
+
+QUnit.test("add cage and solve", (assert: any) => {
     const root = document.createElement("div");
     const ui = new SudokuUI(root);
+    (root.querySelectorAll("input[name=mode]")[3] as HTMLInputElement).click();
+    ui["cages"]["sumUnderConstruction"] = 10;
 
-    ui["textInput"].value = "123456789".repeat(9);
-    const loadButton = root.querySelector("div:nth-child(6) > button:nth-child(1)") as HTMLButtonElement;
-    assert.equal(loadButton.textContent, "Load from text");
-    loadButton.click();
+    for (let c = 0; c < 4; c++) {
+        ui["boardUI"]["_mode"]!.onMouseDown(0, c, new MouseEvent(""));
+    }
+    assert.ok(root.innerHTML.includes("under-construction"));
 
-    assert.equal(root.innerHTML.match(/color: rgba\(152, 251, 152, 0\.5\)/g), null);
-    // +1 for the highlight button
-    assert.equal((root.innerHTML.match(/color: rgba\(0, 0, 0, 0\)/g) ?? []).length, 9 * 9 + 1);
+    (root.querySelector(".options button") as HTMLButtonElement).click();
+    assert.notOk(root.innerHTML.includes("under-construction"));
+    assert.ok(root.innerHTML.includes("polygon"));
 
-    const find2Button = root.querySelector(".find > button:nth-child(2)") as HTMLButtonElement;
-    assert.equal(find2Button.textContent, "2");
-    find2Button.click();
+    const allButton = root.querySelector(".stepControl > button:nth-child(6)") as HTMLButtonElement;
+    assert.equal(allButton.textContent, "All");
+    allButton.click();
 
-    assert.equal(count(root.innerHTML, /color: rgba\(3, 192, 60, 0\.5\)/g), 9);
-    assert.equal(count(root.innerHTML, /color: rgba\(0, 0, 0, 0\)/g), 9 * 8 + 1);
+    assert.equal(count(root.innerHTML, />1234</g), 4);
 });
 
-QUnit.test("select and highlight", (assert: any) => {
+QUnit.test("display possible cage sums", (assert: any) => {
     const root = document.createElement("div");
     const ui = new SudokuUI(root);
 
+    (root.querySelectorAll("input[name=mode]")[3] as HTMLInputElement).click();
+    ui["cages"]["sumUnderConstruction"] = 12;
+    for (let c = 0; c < 4; c++) {
+        ui["boardUI"]["_mode"]!.onMouseDown(0, c, new MouseEvent(""));
+    }
+    (root.querySelector(".options button") as HTMLButtonElement).click();
+    assert.ok(root.innerHTML.includes("polygon"));
+
+    (root.querySelectorAll("input[name=mode]")[5] as HTMLInputElement).click();
     ui["boardUI"]["_mode"]!.onMouseDown(0, 0, new MouseEvent(""));
-    ui["boardUI"]["_mode"]!.onDrag(1, 0, new MouseEvent(""));
-    ui["boardUI"]["_mode"]!.onDrag(2, 0, new MouseEvent(""));
 
-    assert.equal(count(root.innerHTML, /color: rgba\(255, 215, 0, 0\.5\)/g), 3);
-
-    const highlightButton = root.querySelector(".highlight > button:nth-child(2)") as HTMLButtonElement;
-    highlightButton.click();
-
-    assert.equal(count(root.innerHTML, /color: rgba\(255, 215, 0, 0\.5\)/g), 0);
-    assert.equal(count(root.innerHTML, /color: rgba\(204, 172, 0, 0\.624\)/g), 3);
-
-    ui["boardUI"]["_mode"]!.onMouseDown(8, 8, new MouseEvent(""));
-
-    assert.equal(count(root.innerHTML, /color: rgba\(255, 215, 0, 0\.5\)/g), 1);
-    assert.equal(count(root.innerHTML, /color: rgba\(0, 0, 0, 0\.25\)/g), 3 + 1);
+    assert.ok(root.innerHTML.includes(">1236<br />1245<"));
 });
