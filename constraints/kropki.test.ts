ce0db2aeece7927347db5078a77044235d83ab40
Jing Wang <99jingw@gmail.com>
2026-08-09 19:44:44 -0700

    Brute force kropki dots

--- constraints/kropki.test.ts
+++ constraints/kropki.test.ts
@@ -1,107 +1,10 @@
 import * as sudoku from "../sudoku.js";
-import {
-  ConsecutiveKropkiDots,
-  DoubleKropkiDots,
-  shiftDivide,
-  shiftMultiply,
-} from "./kropki.js";
+import { ConsecutiveKropkiDots, DoubleKropkiDots } from "./kropki.js";
 import { processSettings } from "./processor.js";
 
 declare const QUnit: any;
 
 QUnit.module("constraints/kropki");
-
-QUnit.test("shiftMultiply 1-based", (assert: any) => {
-  assert.equal(
-    sudoku.dumpBitSet(shiftMultiply(sudoku.emptyCell(9), 1, 9, 1)),
-    "[123456789]",
-  );
-  assert.equal(
-    sudoku.dumpBitSet(shiftMultiply(sudoku.emptyCell(9), 2, 9, 1)),
-    "[ 2 4 6 8 ]",
-  );
-  assert.equal(
-    sudoku.dumpBitSet(shiftMultiply(sudoku.emptyCell(9), 3, 9, 1)),
-    "[  3  6  9]",
-  );
-  assert.equal(
-    sudoku.dumpBitSet(shiftMultiply(sudoku.bitMask(3), 2, 9, 1)),
-    "[     6   ]",
-  );
-  assert.equal(
-    sudoku.dumpBitSet(shiftMultiply(sudoku.bitMask(6), 2, 9, 1)),
-    "[         ]",
-  );
-});
-
-QUnit.test("shiftMultiply 0-based", (assert: any) => {
-  assert.equal(
-    sudoku.dumpBitSet(shiftMultiply(sudoku.emptyCell(9), 1, 9, 0), 0),
-    "[012345678]",
-  );
-  assert.equal(
-    sudoku.dumpBitSet(shiftMultiply(sudoku.emptyCell(9), 2, 9, 0), 0),
-    "[0 2 4 6 8]",
-  );
-  assert.equal(
-    sudoku.dumpBitSet(shiftMultiply(sudoku.emptyCell(9), 3, 9, 0), 0),
-    "[0  3  6  ]",
-  );
-  assert.equal(
-    sudoku.dumpBitSet(shiftMultiply(sudoku.bitMask(3 + 1), 2, 9, 0), 0),
-    "[      6  ]",
-  );
-  assert.equal(
-    sudoku.dumpBitSet(shiftMultiply(sudoku.bitMask(6 + 1), 2, 9, 0), 0),
-    "[         ]",
-  );
-});
-
-QUnit.test("shiftDivide 1-based", (assert: any) => {
-  assert.equal(
-    sudoku.dumpBitSet(shiftDivide(sudoku.emptyCell(9), 1, 9, 1)),
-    "[123456789]",
-  );
-  assert.equal(
-    sudoku.dumpBitSet(shiftDivide(sudoku.emptyCell(9), 2, 9, 1)),
-    "[1234     ]",
-  );
-  assert.equal(
-    sudoku.dumpBitSet(shiftDivide(sudoku.emptyCell(9), 3, 9, 1)),
-    "[123      ]",
-  );
-  assert.equal(
-    sudoku.dumpBitSet(shiftDivide(sudoku.bitMask(3), 2, 9, 1)),
-    "[         ]",
-  );
-  assert.equal(
-    sudoku.dumpBitSet(shiftDivide(sudoku.bitMask(6), 2, 9, 1)),
-    "[  3      ]",
-  );
-});
-
-QUnit.test("shiftDivide 0-based", (assert: any) => {
-  assert.equal(
-    sudoku.dumpBitSet(shiftDivide(sudoku.emptyCell(9), 1, 9, 0), 0),
-    "[012345678]",
-  );
-  assert.equal(
-    sudoku.dumpBitSet(shiftDivide(sudoku.emptyCell(9), 2, 9, 0), 0),
-    "[01234    ]",
-  );
-  assert.equal(
-    sudoku.dumpBitSet(shiftDivide(sudoku.emptyCell(9), 3, 9, 0), 0),
-    "[012      ]",
-  );
-  assert.equal(
-    sudoku.dumpBitSet(shiftDivide(sudoku.bitMask(3 + 1), 2, 9, 0), 0),
-    "[         ]",
-  );
-  assert.equal(
-    sudoku.dumpBitSet(shiftDivide(sudoku.bitMask(6 + 1), 2, 9, 0), 0),
-    "[   3     ]",
-  );
-});
 
 QUnit.test("eliminate kropki", (assert: any) => {
   const constraints = [
--- constraints/kropki.ts
+++ constraints/kropki.ts
@@ -1,113 +1,30 @@
-import {
-  ALL_ONES,
-  Board,
-  Coordinate,
-  ReadonlyBoard,
-  bitMask,
-} from "../sudoku.js";
-import { ProcessedSettings } from "./constraint.js";
-import { Constraint } from "./constraint.js";
+import { BruteForceConstraint } from "./constraint.js";
 
-/**
- * Compute shift(set1, 0) & shift(set2, 1) & shift(set3, 2) & ...
- */
-function intersectWithShift(
-  board: ReadonlyBoard,
-  coordinates: readonly Coordinate[],
-  shift: (set: number, i: number) => number,
-): number {
-  let start = ALL_ONES;
-  for (let i = 0; i < coordinates.length; i++) {
-    const [r, c] = coordinates[i];
-    start &= shift(board[r][c], i);
+function checkDifference(digits: number[], diff: number): boolean {
+  for (let i = 1; i < digits.length; i++) {
+    if (digits[i] - digits[i - 1] !== diff) {
+      return false;
+    }
   }
-  return start;
+  return true;
+}
+function checkMultiple(digits: number[], multiple: number): boolean {
+  for (let i = 1; i < digits.length; i++) {
+    if (digits[i] / digits[i - 1] !== multiple) {
+      return false;
+    }
+  }
+  return true;
 }
 
-// A single KropkiDots constraint represents a chain of dots where digits cannot repeat
-
-export class ConsecutiveKropkiDots extends Constraint {
-  performElimination(
-    _: ProcessedSettings,
-    origBoard: ReadonlyBoard,
-    board: Board,
-  ): void {
-    // get all possible values for first cell if ascending or descending
-    const startAscending = intersectWithShift(
-      origBoard,
-      this.members,
-      (s, i) => s >>> i,
-    );
-    const startDescending = intersectWithShift(
-      origBoard,
-      this.members,
-      (s, i) => s << i,
-    );
-    // apply to whole chain
-    for (let i = 0; i < this.members.length; i++) {
-      const [r, c] = this.members[i];
-      board[r][c] &= (startAscending << i) | (startDescending >>> i);
-    }
+export class ConsecutiveKropkiDots extends BruteForceConstraint {
+  isValid(digits: number[]): boolean {
+    return checkDifference(digits, 1) || checkDifference(digits, -1);
   }
 }
 
-/* Return a new set where each member of the original set is multiplied by factor */
-export function shiftMultiply(
-  set: number,
-  factor: number,
-  boardSize: number,
-  startDigit: number,
-): number {
-  const offset = startDigit - 1;
-  let result = 0;
-  for (let d = 1; d <= boardSize; d++) {
-    const newDigit = (d + offset) * factor - offset;
-    if (set & bitMask(d) && newDigit <= boardSize) {
-      result |= bitMask(newDigit);
-    }
-  }
-  return result;
-}
-
-/* Return a new set where each member of the original set is divided by factor */
-export function shiftDivide(
-  set: number,
-  factor: number,
-  boardSize: number,
-  startDigit: number,
-): number {
-  const offset = startDigit - 1;
-  let result = 0;
-  for (let d = 1; d <= boardSize; d++) {
-    if (set & bitMask(d) && (d + offset) % factor === 0) {
-      result |= bitMask((d + offset) / factor - offset);
-    }
-  }
-  return result;
-}
-
-export class DoubleKropkiDots extends Constraint {
-  performElimination(
-    settings: ProcessedSettings,
-    origBoard: ReadonlyBoard,
-    board: Board,
-  ): void {
-    const startDigit = settings.startDigit;
-    // get all possible values for first cell if ascending or descending
-    const startAscending = intersectWithShift(origBoard, this.members, (s, i) =>
-      shiftDivide(s, 1 << i, board.length, startDigit),
-    );
-    const startDescending = intersectWithShift(
-      origBoard,
-      this.members,
-      (s, i) => shiftMultiply(s, 1 << i, board.length, startDigit),
-    );
-    // apply to whole chain
-    for (let i = 0; i < this.members.length; i++) {
-      const [r, c] = this.members[i];
-      board[r][c] &=
-        shiftMultiply(startAscending, 1 << i, board.length, startDigit) |
-        shiftDivide(startDescending, 1 << i, board.length, startDigit);
-    }
+export class DoubleKropkiDots extends BruteForceConstraint {
+  isValid(digits: number[]): boolean {
+    return checkMultiple(digits, 2) || checkMultiple(digits, 0.5);
   }
 }
